f2f4222f46166a625b769a6fb271c628458ada5b
colinclerk <colin@clerk.dev>
2022-02-16 13:48:30 -0500

    First commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ClerkProvider>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
